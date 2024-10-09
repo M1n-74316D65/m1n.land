911b5c9a17269f3978d7c985280af1a4cc88f719
M1n <54779580+M1n-74316D65@users.noreply.github.com>
2024-10-10 01:04:54 +0200

    refactor(ui): update BentoCard component to use Next.js Link

--- app/components/ui/bento-grid.tsx
+++ app/components/ui/bento-grid.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from "react";
 import { ArrowRightIcon } from "@radix-ui/react-icons";
+import Link from "next/link";
 
 import { cn } from "app/lib/utils";
 import { Button } from "app/components/ui/button";
@@ -66,10 +67,10 @@ const BentoCard = ({
       )}
     >
       <Button variant="ghost" asChild size="sm" className="pointer-events-auto">
-        <a href={href}>
+        <Link href={href} target="_blank" rel="noopener noreferrer">
           {cta}
           <ArrowRightIcon className="ml-2 h-4 w-4" />
-        </a>
+        </Link>
       </Button>
     </div>
     <div className="pointer-events-none absolute inset-0 transform-gpu transition-all duration-300 group-hover:bg-black/[.03] group-hover:dark:bg-neutral-800/10" />

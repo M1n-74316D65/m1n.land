6e5dd5b6f085dd88419884f7c92b75e5cadb21a9
David <git@m1n.land>
2025-07-06 12:40:04 +0200

    refactor: remove links page and update navigation items

--- app/constants/navItems.ts
+++ app/constants/navItems.ts
@@ -2,7 +2,6 @@ export const navItems = [
   { path: "/", name: "home" },
   { path: "/projects", name: "projects" },
   { path: "https://journal.m1n.land", name: "blog" },
-  { path: "/links", name: "links" },
   { path: "/radio", name: "radio" },
   { path: "/guestbook", name: "guestbook" },
 ];

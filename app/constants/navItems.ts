1a99f2b0bab16cfaa28508450314b0224d1eeea6
m1n <git@m1n.land>
2026-01-04 13:45:27 +0100

    fix: remove radio and guest

--- app/constants/navItems.ts
+++ app/constants/navItems.ts
@@ -2,6 +2,6 @@ export const navItems = [
   { path: '/', name: 'home' },
   { path: '/projects', name: 'projects' },
   { path: 'https://journal.m1n.land', name: 'blog' },
-  { path: '/radio', name: 'radio' },
-  { path: '/guestbook', name: 'guestbook' },
+  // { path: '/radio', name: 'radio' },
+  // { path: '/guestbook', name: 'guestbook' },
 ]
